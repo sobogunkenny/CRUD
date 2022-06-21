8afaf6cc762dec3f82a6cf264878b534db628428
sobogunkenny <kekeke4coolguy@yahoo.com>
2022-06-21 20:39:15 +0100

    Committed

---
+++ sobogunkenny/blog/views.py
@@ -0,0 +1,26 @@
+
+from django.views.generic.edit import ListView, CreateView, DetailView, UpdateView 
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
